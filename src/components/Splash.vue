96c980cff3dcdff80934c4fb36a4fc587a598ec8
Anthony Norman <anthnorm@gmail.com>
2020-12-14 10:15:48 +0100

    Setup

---
+++ src/components/Splash.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="container my-5">
+  <!-- Section: Block Content -->
+  <section class="dark-grey-text text-center">
+
+    <h3 class="text-center font-weight-bold mb-4 pb-2">Shopping made for you in mind</h3>
+    <p class="text-center text-muted w-responsive mx-auto mb-5">Choose your interest of today and pick it.</p>
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-12 mb-4">
+
+        <!-- Card -->
+        <div class="card">
+          <div class="text-white text-center d-flex align-items-center  py-5 px-4 px-md-5 rounded">
+            <div>
+              <h6 class="ligthpurple-text">
+                <!-- <i class="fas fa-plane"></i> -->
+                <strong>EXCESSIT pick n choose</strong>
+              </h6>
+              <h3 class="py-3 font-weight-bold"><strong>Shopping made for you in mind</strong></h3>
+                <carousel />
+              
+              <p class="pb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat,
+                laboriosam, voluptatem, optio vero odio nam sit officia accusamus minus error nisi architecto
+                nulla ipsum dignissimos. Odit sed qui, dolorum!
+              </p>
+              <a class="btn btn-secondary btn-rounded btn-md"><i class="fas fa-clone left"></i> View project</a>
+            </div>
+          </div>
+        </div>
+        <!-- Card -->
+      </div>
+      <!-- Grid column -->
+    </div>
+    <!-- Grid row -->
+
+  </section>
+  <!-- Section: Block Content -->
+</div>
+</template>
+
+<script>
+import Carousel from './Carousel'
+
+export default {
+    name: 'Splash',
+    components: {
+      Carousel
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/private/NavbarP.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+mongodb+srv://Anthony:BytMig123@cluster0.de07g.mongodb.net/excessitPT?authSource=admin&replicaSet=atlas-2z77qt-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
+mongodb+srv://Anthony:BytMig123@cluster0.de07g.mongodb.net/excessitPT?retryWrites=true&w=majority
